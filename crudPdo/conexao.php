dcb30b525519158579a00a9c982772229d08bd2b
renatoguara2020 <renatoguara2020@gmail.com>
2023-05-03 10:12:12 -0300

    OK

--- crudPdo/conexao.php
+++ crudPdo/conexao.php
@@ -4,6 +4,8 @@ try {
 
 	
 	$conexao = new PDO("mysql:dbname=crud;host=localhost", "root", "");
+	$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 	
 } catch (Exception $e) {
 	echo "Erro ao conectar com o Banco de Dados! ".$e;
